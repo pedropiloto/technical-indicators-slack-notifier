44c1d204bd3d09732d27de46b921fdb8a3473d0d
Pedro Piloto <pedro@pedropiloto.com>
2021-02-22 21:00:21 +0000

    rate limit

--- app/gateways/finnhub-gateway.js
+++ app/gateways/finnhub-gateway.js
@@ -8,13 +8,13 @@ const headers = {};
 headers['X-Finnhub-Token'] = finnhubToken;
 
 const limiter = new Bottleneck({
-  reservoir: 40, // initial value
-  reservoirRefreshAmount: 40,
+  reservoir: 20, // initial value
+  reservoirRefreshAmount: 20,
   reservoirRefreshInterval: 60 * 1000, // must be divisible by 250
 
   // also use maxConcurrent and/or minTime for safety
   maxConcurrent: 1,
-  minTime: 1000, // pick a value that makes sense for your use case
+  minTime: 1600, // pick a value that makes sense for your use case
 });
 
 // eslint-disable-next-line import/prefer-default-export
